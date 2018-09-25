5d6f068b0a24630e6d7b4ae299e54a1cc3211822
Connie Lim <jlim13@binghamton.edu>
2018-09-25 16:53:44 -0400

    commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+const buildMarker = require("./marker");
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = "pk.eyJ1IjoiYW5uYWthcGxhbiIsImEiOiJjam1pNWdpbjIwMWd2M3FxbGF3cmwzOWJjIn0.WYqFHM1eukNSnB5AkE7WZQ";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
+
+// const markerDomEl = document.createElement("div"); // Create a new, detached DIV
+// markerDomEl.style.width = "32px";
+// markerDomEl.style.height = "39px";
+// markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+
+// new mapboxgl.Marker(markerDomEl).setLngLat([-74.009, 40.705]).addTo(map); // [-87.641, 41.895] for Chicago
+
+const marker = buildMarker("activity", [-74.009151, 40.705086]); // or [-87.6354, 41.8885]
